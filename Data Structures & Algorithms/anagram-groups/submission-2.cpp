4d5f49b181c40e88621c24e013162d3da0b85927
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-06-15 20:57:23 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+        vector<vector<string>> answer;
+
+        for(auto &it: strs) {
+            string org = it;
+            sort(it.begin(), it.end());
+            mpp[it].push_back(org);
+        }
+
+        for(auto &it: mpp) {
+            answer.push_back(it.second);
+        }
+
+        return answer;
+    }
+};
