24efbd854a849ddd50379577570b1b91a01d8a5f
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-06-20 16:08:20 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    unordered_map<char, string> mpp = {
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"}
+    };
+    vector<string> answer;
+    string subset;
+
+    void dfs(int index, string digits) {
+        if(subset.size() == digits.size()) {
+            answer.push_back(subset);
+            return;
+        }
+
+        for(char ch: mpp[digits[index]]) {
+            subset.push_back(ch);
+
+            dfs(index + 1, digits);
+
+            subset.pop_back();
+        }
+    }
+
+
+    vector<string> letterCombinations(string digits) {
+        if(digits == "") {
+            return answer;
+        }
+        dfs(0, digits);
+
+        return answer;
+    }
+};
