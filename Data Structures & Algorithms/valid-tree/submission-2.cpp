2e4f51dfa65724fc2a4872b09c2b8ac9ee6231a8
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-06-24 17:55:24 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    vector<bool> visited;
+    int count = 0;
+    vector<vector<int>> graph;
+    int num;
+
+    bool dfs(int node, int parent) {
+        count++;
+        visited[node] = true;
+
+        for(int nei: graph[node]) {
+
+            if(nei == parent) {
+                continue;
+            }
+
+            if(visited[nei]) {
+                return true;
+            }
+
+            if(dfs(nei, node)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        visited = vector<bool>(n, false);
+        graph = vector<vector<int>>(n);
+
+        for(auto it: edges) {
+            int node1 = it[0];
+            int node2 = it[1];
+            graph[node1].push_back(node2);
+            graph[node2].push_back(node1);
+        }
+
+        if(dfs(0, -1)) {
+            return false;
+        }
+
+        return count == n;
+    }
+};
