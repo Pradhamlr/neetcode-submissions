66edc0ef9251ee6787519ddee065717182598179
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-07-05 14:40:30 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s = "";
+        for(auto it: strs) {
+            s.append(to_string(it.length()));
+            s.append("#");
+            s.append(it);
+        }
+        cout << s;
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> answer;
+        int i = 0;
+        string length;
+        while(i < s.length()) {
+            if(s[i] != '#') {
+                length += s[i];
+                i++;
+            }
+            if(s[i] == '#') {
+                int len = stoi(length);
+                i++;
+                answer.push_back(s.substr(i, len));
+                length = "";
+                i += len;
+            }
+        }
+        return answer;
+    }
+};
