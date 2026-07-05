842cb80da5cb943f4ce173a6a4329292b778ccec
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-07-05 14:03:11 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,34 @@
+class TimeMap {
+public:
+
+    unordered_map<string, vector<pair<string, int>>> mpp;
+
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mpp[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        auto& arr = mpp[key];
+        int l = 0;
+        int r = arr.size() - 1;
+        string answer = "";
+
+        while(l <= r) {
+            int mid = (l + r)/2;
+
+            if(arr[mid].second <= timestamp) {
+                answer = arr[mid].first;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+};
